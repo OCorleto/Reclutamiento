d2e10f89bac2cf5a97b147287e5a5447a95da27e
Oscar Corleto <oscarcorleto000@gmail.com>
2021-02-23 12:06:06 -0600

    Mostrar peliculas individuales

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-var mongo = require('mongoose');
+const dirip = "http://localhost:3000"
+
 const MongoClient = require('mongodb').MongoClient
 const db_path = 'mongodb://localhost:27017/ilu'
 const db_config = {
@@ -21,10 +22,20 @@ MongoClient.connect(db_path,function(err,client){
 router.get('/',(req,res)=>{
     peliculas = db.collection("pelicula").find({en_cartelera: 1}).toArray(function(err, result) {
         if (err) throw err;
-        res.render('index.html',{pelis: result})
-      });
-    
+        res.render('index.html',{pelis: result,direccion:dirip})
+    });
 })
 
+router.get('/info/:id',(req,res)=>{
+    var id = parseInt(req.params.id)
+    console.log("id de pelicula "+id)
+    const query = { "id_peli": id };
+    db.collection("pelicula").findOne(query)
+    .then(result => {
+      if(result) res.render('info.html',{peli: result,direccion:dirip});
+      else res.send("Película no encontrada")
+    })
+    .catch(err => res.send(`Failed to find document: ${err}`));
+})
 
 module.exports = router
